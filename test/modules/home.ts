145bc365fb5cf9f9e8ba51a90d747c289a849c1a
Daniel Welsh <d.welsh@newcastle.ac.uk>
2019-01-25 16:34:41 +0100

    :white_check_mark: Adding tests for checking docs

--- test/modules/home.ts
+++ test/modules/home.ts
@@ -4,14 +4,14 @@ import axios, { AxiosResponse } from 'axios';
 import { URL } from '../commons';
 import { expect } from 'chai';
 
-//describe('Home', () => {
-//  // Test the landing page renders
-//  describe('Render', () => {
-//    it("Should return the home page from '/'", (done) => {
-//      axios.get(`${URL}/`).then((response: AxiosResponse) => {
-//        expect(response.status).to.equal(200);
-//        done();
-//      });
-//    });
-//  });
-//});
+describe('Home', () => {
+ // Test the landing page renders
+ describe('Render', () => {
+   it("Should return the home page from '/'", (done) => {
+     axios.get(`${URL}/`).then((response: AxiosResponse) => {
+       expect(response.status).to.equal(200);
+       done();
+     });
+   });
+ });
+});
